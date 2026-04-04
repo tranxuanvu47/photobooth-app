ded2774ce629455b5210e28dd989323965f3c435
admin <nguyenduylinhbnbmt98@gmail.com>
2026-04-04 22:00:44 +0700

    Update Admin settings: Print control, Auto-Return toggle, and Gallery limit

--- push_code.bat
+++ push_code.bat
@@ -1,7 +1,8 @@
 @echo off
 set TOKEN=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
 set REMOTE_URL=https://%TOKEN%@github.com/tranxuanvu47/photobooth-app.git
-set BRANCH=teonui
+set LOCAL_BRANCH=main
+set REMOTE_BRANCH=teonui
 
 echo --- Staging changes ---
 git add .
@@ -12,8 +13,8 @@ git commit -m "Update Admin settings: Print control, Auto-Return toggle, and Gal
 echo --- Setting remote URL with authentication ---
 git remote set-url origin %REMOTE_URL%
 
-echo --- Pushing to branch: %BRANCH% ---
-git push origin %BRANCH%
+echo --- Pushing current branch (%LOCAL_BRANCH%) to remote branch (%REMOTE_BRANCH%) ---
+git push origin %LOCAL_BRANCH%:%REMOTE_BRANCH%
 
 echo --- Done ---
 pause
